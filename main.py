91637ea6cccbd0eaf6756f33aeaa0e4b708b0f65
StSav012 <stsav012@gmail.com>
2020-08-27 15:39:57 +0300

    Added check that PyQt5 is supported

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
+import sys
+
 try:
     import gui
 except ImportError as ex:
-    import sys
-
     tb = sys.exc_info()[2]
     print(ex.with_traceback(tb))
     print('Ensure that PyQt5-sip and PyQt5 are installed')
@@ -11,4 +11,14 @@ except SyntaxError:
     print('Get a newer Python!')
 else:
     if __name__ == '__main__':
-        gui.run()
+        import subprocess
+
+        child = subprocess.Popen([sys.executable, '-c',
+                                  'from PyQt5.QtWidgets import QApplication; QApplication([])'],
+                                 stderr=subprocess.PIPE)
+        child.wait()
+        ret_code = child.returncode
+        if ret_code == 0:
+            gui.run()
+        else:
+            print('This platform is not supported by Qt5')
