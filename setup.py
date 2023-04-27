5817f40f3c993e5182034b843821d33e56e2cdbd
stsav012 <stsav012@gmail.com>
2023-04-27 21:51:50 +0300

    Fix Qt package selection

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 # coding=utf-8
 from __future__ import annotations
 
-from functools import lru_cache
+import site
 
-from setuptools import build_meta as _orig
-from setuptools.build_meta import *  # type: ignore
+from setuptools import setup
+
+site.main()
 
 
-@lru_cache(maxsize=None, typed=True)
 def required_packages() -> list[str]:
     import sys
     if sys.version_info < (3, 8):
-        return []
+        raise SystemError('Python versions prior to 3.8 are not supported')
 
     import platform
     from typing import NamedTuple, Sequence
@@ -20,21 +20,19 @@ def required_packages() -> list[str]:
 
     class PackageRequirement(NamedTuple):
         package_name: str
-        import_name: str
         min_version: str = ''
 
         def __str__(self) -> str:
             if self.min_version:
-                return self.package_name + '>=' + self.min_version
+                return self.package_name + ' >= ' + self.min_version
             return self.package_name
 
     def is_package_importable(package_requirement: PackageRequirement) -> bool:
-        from importlib import import_module
-        from importlib.metadata import version
+        from importlib.metadata import version, PackageNotFoundError
 
         try:
-            import_module(package_requirement.import_name)
-        except (ModuleNotFoundError,):
+            version(package_requirement.package_name)
+        except PackageNotFoundError:
             return False
         else:
             if (package_requirement.min_version
@@ -55,17 +53,18 @@ def required_packages() -> list[str]:
         if not package_requirement:
             raise ValueError('No package requirements given')
 
-        if isinstance(package_requirement, PackageRequirement) and is_package_importable(package_requirement):
-            return package_requirement
-
         if not isinstance(package_requirement, PackageRequirement) and isinstance(package_requirement, Sequence):
             for _package_requirement in package_requirement:
                 if is_package_importable(_package_requirement):
                     return _package_requirement
-
-        if isinstance(package_requirement, Sequence):
             return package_requirement[0]
-        return package_requirement
+
+        if isinstance(package_requirement, PackageRequirement):
+            if is_package_importable(package_requirement):
+                return package_requirement
+            else:
+                return package_requirement
+        raise ValueError('Invalid requirements')
 
     qt_list: list[PackageRequirement]
     uname: platform.uname_result = platform.uname()
@@ -73,26 +72,23 @@ def required_packages() -> list[str]:
          and parse_version(uname.version) < parse_version('10.0.19044'))  # Windows 10 21H2 or later required
             or uname.machine not in ('x86_64', 'AMD64')):
         # Qt6 does not support the OSes
-        qt_list = [PackageRequirement(package_name='PyQt5', import_name='PyQt5.QtCore')]
+        qt_list = [
+            PackageRequirement(package_name='PyQt5'),
+            PackageRequirement(package_name='PySide2'),
+        ]
     else:
         qt_list = [
-            PackageRequirement(package_name='PySide6-Essentials', import_name='PySide6.QtCore'),
-            PackageRequirement(package_name='PyQt6', import_name='PyQt6.QtCore'),
-            PackageRequirement(package_name='PyQt5', import_name='PyQt5.QtCore'),
+            PackageRequirement(package_name='PySide6-Essentials', min_version='6.2.0'),
+            PackageRequirement(package_name='PyQt6', min_version='6.2.0'),
+            PackageRequirement(package_name='PyQt5', min_version='5.12.0'),
+            PackageRequirement(package_name='PySide2', min_version='5.12.0'),
         ]
-    if sys.version_info < (3, 11):  # PySide2 from pypi is not available for Python 3.11 and newer
-        qt_list.append(PackageRequirement(package_name='PySide2', import_name='PySide2.QtCore'))
 
     requirements: list[PackageRequirement | Sequence[PackageRequirement]] = [
-        PackageRequirement(package_name='qtpy', import_name='qtpy', min_version='2.3.1'),
+        PackageRequirement(package_name='qtpy', min_version='2.3.1'),
         qt_list,
     ]
     return [str(required_package(requirement)) for requirement in requirements]
 
 
-def get_requires_for_build_sdist(config_settings=None):
-    return _orig.get_requires_for_build_sdist(config_settings) + required_packages()
-
-
-def get_requires_for_build_wheel(config_settings=None):
-    return _orig.get_requires_for_build_wheel(config_settings) + required_packages()
+setup(install_requires=required_packages())
