585730f895ac205c9734f7a9e3db6bd1d47bec60
StSav012 <stsav012@gmail.com>
2025-08-14 19:47:50 +0300

    Fix frequency ranges merging

--- src/pycatsearch/catalog.py
+++ src/pycatsearch/catalog.py
@@ -129,6 +129,7 @@ class CatalogData:
             if not args:
                 return tuple()
             ranges: tuple[tuple[float, float], ...] = tuple()
+            args = sorted(args)
             skip: int = 0
             for i in range(len(args)):
                 if skip > 0:
