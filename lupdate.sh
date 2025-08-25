046bf27beccfdbad1d315447c270e96a704ffa30
StSav012 <stsav012@gmail.com>
2025-08-25 15:42:46 +0300

    Fix source files not found

--- lupdate.sh
+++ lupdate.sh
@@ -3,5 +3,6 @@ L_UPDATE="../qt-linguist/lupdate.py"
 if [[ -e ${L_UPDATE} ]]; then L_UPDATE="python ${L_UPDATE}"; else L_UPDATE="pyside6-lupdate"; fi
 echo using "${L_UPDATE}"
 for n in src/pycatsearch/gui/i18n/*.ts; do
-  ${L_UPDATE} "${py_files}" -ts "${n}" "$@"
+  # shellcheck disable=SC2086
+  ${L_UPDATE} ${py_files} -ts "${n}" "$@"
 done
