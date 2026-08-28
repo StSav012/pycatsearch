d4d85d79bac3b5ad388488f2556b44ef8297c30f
Anton Yablokov <stsav012@gmail.com>
2026-08-28 17:32:20 +0300

    Change `sys.argv` for a test

--- test/test_async_download.py
+++ test/test_async_download.py
@@ -1,16 +1,18 @@
-#!/usr/bin/env python3
+import sys
+from collections.abc import Iterable
 
 
-def test_async_download():
+def test_async_download(args: Iterable[str] = ()):
     from src.pycatsearch import async_download
+
+    orig_args: list[str] = sys.argv.copy()
+    sys.argv.extend(args)
 
     async_download()
 
+    sys.argv = orig_args
+
 
 if __name__ == "__main__":
-    import sys
-    from os import path
-
-    sys.path = list(set(sys.path) | {path.abspath(path.join(__file__, path.pardir))})
-
-    test_async_download()
+    test_async_download(["-V"])
+    test_async_download(["-fmin", "110000", "-fmax", "184000", "test_download_catalog.json.gz"])
--- test/test_download.py
+++ test/test_download.py
@@ -1,22 +1,18 @@
-#!/usr/bin/env python3
+import sys
 from collections.abc import Iterable
 
 
 def test_download(args: Iterable[str] = ()):
-    import sys
-
     from src.pycatsearch import download
 
+    orig_args: list[str] = sys.argv.copy()
     sys.argv.extend(args)
 
     download()
 
+    sys.argv = orig_args
+
 
 if __name__ == "__main__":
-    import sys
-    from os import path
-
-    sys.path = list(set(sys.path) | {path.abspath(path.join(__file__, path.pardir))})
-
     test_download(["-V"])
     test_download(["-fmin", "110000", "-fmax", "184000", "test_download_catalog.tar.gz"])
